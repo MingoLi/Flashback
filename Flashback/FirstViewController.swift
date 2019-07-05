3e98ca2b1c480a2263b5ca4c7e27bd9c1b57d92e
Minghao Li <lim34521@myumanitoba.ca>
2019-07-04 20:17:04 -0500

    Initial Commit

---
+++ Flashback/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Flashback
+//
+//  Created by Mingo on 2019-07-04.
+//  Copyright © 2019 Mingo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flashback/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Flashback
+//
+//  Created by Mingo on 2019-07-04.
+//  Copyright © 2019 Mingo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
